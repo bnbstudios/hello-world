47624f3f1e62e4f3a696bad7c6dd2c599fc27bd9
bnbstudios <bnbstudios@gmail.com>
2016-07-17 23:16:12 +0545

    Add files via upload
    new files added

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\bin\AndroidManifest.xml : \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\AndroidManifest.xml \
---
+++ gen/R.java.d
@@ -0,0 +1,14 @@
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\gen\com\company\and1\R.java \
+ : C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-hdpi\icon.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-hdpi\splash.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-ldpi\icon.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-ldpi\splash.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-mdpi\icon.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-mdpi\splash.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\layout\main.xml \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\res\values\strings.xml \
+C:\Users\Bishal\DOCUME~1\Desktop\COMCOM~1.AND\bin\AndroidManifest.xml \
